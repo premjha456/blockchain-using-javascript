73f0c179e5274a46e00868249244f004b7f1156b
Prem <2018premkishor.jha@ves.ac.in>
2019-03-18 23:57:44 +0530

    some additional methods

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -248,6 +248,39 @@ app.get('/consensus',function(req,res){
     });
 });
 
+
+app.get('/block/:blockHash',function(req,res){
+  
+    const blockHash = req.params.blockHash;
+    const correctBlock= mycoin.getBlock(blockHash);
+
+    res.json({
+        block:correctBlock
+    });
+});
+
+app.get('/transaction/:transactionId',function(req,res){
+
+    const transactionId = req.params.transactionId;
+    const transactionData= mycoin.getTransaction(transactionId);
+
+    res.json({
+        transaction:transactionData.transaction,
+        block:transactionData.block
+    });
+});
+
+app.get('/address/:address',function(req,res){
+
+    const address = req.params.address;
+    const addressData= mycoin.getAddress(address);
+
+    res.json({
+        addressData:addressData
+    });
+
+});
+
 app.listen(port,function(){
     console.log(`Server running on port ${port}`);
 });
